5dcae43810ab44dda2cb0fe0c070b98fc872b0e5
ffthkrdg <ffthkrdg@gmail.com>
2020-08-14 04:27:40 +0300

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCvnlH6ytg6Lvcj-w7VESljJWN5--fNcUM",
+  authDomain: "netflix-clone-c6037.firebaseapp.com",
+  databaseURL: "https://netflix-clone-c6037.firebaseio.com",
+  projectId: "netflix-clone-c6037",
+  storageBucket: "netflix-clone-c6037.appspot.com",
+  messagingSenderId: "839282511385",
+  appId: "1:839282511385:web:170045981f5f617abd6488",
+  measurementId: "G-4PRDWJ9TFE",
+};
